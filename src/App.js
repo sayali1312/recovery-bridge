94db9b159055b3d4dadea87c18da1df4d0ff482c
n0vay <koshantanu@gmail.com>
2023-04-21 02:24:51 -0400

    added pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Settings } from "@mui/icons-material";
 import Sidenav from "./sidenav";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import home from "./pages/Home"
+import settings from "./pages/Settings"
+import about from "./pages/About"
 
 function App() {
   return <>
-  <Sidenav />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element = {<home />}></Route>
+        <Route path="/about" exact element = {<about />}></Route>
+        <Route path="/settings" exact element = {<settings />}></Route>
+      </Routes>
+    </BrowserRouter>
   </>
 }
 
